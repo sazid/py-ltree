8ebe2015b64856700cd545aa59ba15c89d546ccf
Daniele Varrazzo <piro@gambitresearch.com>
2019-01-04 12:03:53 +0000

    Version and classifiers updated

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="ltree",
-    version="0.0.3",
+    version="0.0.4",
     author="Daniele Varrazzo",
     author_email="daniele.varrazzo@gmail.com",
     description="Python wrapper for the PostgreSQL ltree data type",
@@ -24,8 +24,9 @@ setup(
     classifiers=[
         l.strip()
         for l in """
-        Development Status :: 3 - Alpha
-        Programming Language :: Python :: 2.7, 3.4
+        Development Status :: 4 - Beta
+        Programming Language :: Python :: 2.7
+        Programming Language :: Python :: 3
         Intended Audience :: Developers
         License :: OSI Approved :: BSD License
         Topic :: Database
